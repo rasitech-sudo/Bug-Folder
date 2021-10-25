b2887d1aaaa0248ca347309965fbd406c2977a39
rasitech-sudo <73753579+rasitech-sudo@users.noreply.github.com>
2021-10-25 14:52:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
-<a href="/0.zip">Download Here</a>
-<a href="/rasi.zip">Download Here</a>
+<a href="/0.zip">Download Here</a><br></br>
+<a href="/rasi.zip">Download Here</a><br></br>
 <a href="/0">Start Here</a>
+<?php
+$zip = new ZipArchive;
+  
+// Zip File Name
+if ($zip->open('0.zip') === TRUE) {
+  
+    // Unzip Path
+    $zip->extractTo('./');
+    $zip->close();
+    echo 'Unzipped Process Successful!';
+} else {
+    echo 'Unzipped Process failed';
+}
+?>
